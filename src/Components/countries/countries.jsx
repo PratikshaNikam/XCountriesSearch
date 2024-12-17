980e4a8570f08698eddd7a9f6f6916158f0b65ed
Pratiksha Nikam <pratiksha.gawande@gmail.com>
2024-12-17 13:55:04 +0530

    search box added

--- src/Components/countries/countries.jsx
+++ src/Components/countries/countries.jsx
@@ -1,10 +1,11 @@
-import React,{useEffect,useState} from "react"; 
+import React, { useEffect, useState } from "react"; 
+import styles from './countries.module.css';
 
 function CountryCard({ name, flagurl }){
  //console.log(name)
   return (
-    <div style={{display:"flex", flexDirection:"column", width:"150px",height:"150px", border:"1px solid gray", borderRadius:"10px", justifyContent:"center",alignItems:"center",padding:"10px"}} >
-      <img src={flagurl} alt={name} width="100px" height="100px"/>
+    <div  className={styles.countryCard}>
+      <img src={flagurl} alt={name} width="150px" height="100px"/>
       <h3>{name}</h3>
     </div>
   )
@@ -14,28 +15,45 @@ function CountryCard({ name, flagurl }){
 function Countries() {
   // const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const [countries, setCountries] = useState([])
-  const [search, setSearch] = useState("asd")
+  const [search, setSearch] = useState("")
+  const [filteredCountries, setFilteredCountries] = useState([])
   
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all").then((res) => res.json()).then((data) => setCountries(data))
-    .catch((error)=>console.error("Error fetching data:", + error));
+    .catch((error)=>console.error("Failed to fetch the data", + error));
     
   }, []);
 
-  console.log(search)
+
+  useState(() => {
+    const filteredCountry = countries.filter((country) =>
+      country.name.common.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilteredCountries(filteredCountry);
+  }, [search]);
+
+
+
+
+  console.log(filteredCountries)
  
   return (
    
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "10px" }}>
       
-      <input type="text" placeholder="Search for Countries..." style={{ width: "500px", height: "20px", borderRadius: "10px", border: "1px solid gray", padding: "10px" }} onChange={(e) => { setSearch(e.target.value) }} />
+      <input type="text" placeholder="Search for Countries..." className={styles.img} onChange={(e) => { setSearch(e.target.value) }} />
     
       
     <div style={{display:"flex" ,flexWrap:"wrap",gap:"10px"}}>
-      {countries.map((item) => (
+      {search === ""? countries.map((item) => (
         //console.log(item.flag)
         <CountryCard name={item.name.common} flagurl={item.flags.png} key={item.name.common} />
-      ))}
+      )) :
+          filteredCountries.map((item) => (
+            <CountryCard name={item.name.common} flagurl={item.flags.png} key={item.name.common} />
+          ))
+    
+    }
       </div>
       
       </div>
